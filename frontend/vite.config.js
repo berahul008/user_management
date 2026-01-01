11bc4ab74aa80f22162e0bcadf5047ac20a5d3fd
berahul008 <nakaderahul94@gmail.com>
2026-01-01 09:59:24 +0000

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: {
+    host: true,         // listen on 0.0.0.0
+    port: 3000
+  }
 })
+
